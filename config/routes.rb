82066ca14e8d445437d5604a47af86977cfbc8d0
NEWIN <newin95@likelion.org>
2018-04-27 13:38:50 +0000

    crud 최종완성

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get 'posts/show/:id' => 'posts#show'
   post 'posts/create' => 'posts#create'
   get 'posts/edit/:id' => 'posts#edit'
-  get 'posts/destroy'
+  get 'posts/destroy/:id' => 'posts#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
