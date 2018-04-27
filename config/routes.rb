9a08bd99f1c0b78207c3f10abdf0a2e6227b2d8c
NEWIN <newin95@likelion.org>
2018-04-27 13:24:27 +0000

    수정(u) 완성

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   root 'posts#index'
   get 'posts/index'
   get 'posts/new'
-  get 'posts/update'
+  post 'posts/update/:id' => 'posts#update'
   get 'posts/show/:id' => 'posts#show'
-  post 'posts/create' =>'posts#create'
-  get 'posts/edit'
+  post 'posts/create' => 'posts#create'
+  get 'posts/edit/:id' => 'posts#edit'
   get 'posts/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
